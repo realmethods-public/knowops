f448d6c0971b656deb13a62be99659d5f2a7bf57
Steven Randolph <srandolph@cloudmigrate.io>
2021-01-11 13:54:42 -0600

    use explicit full directory in entrypoint call

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-service mysql start 
-mysql -e "SET PASSWORD FOR 'root'@'localhost'=PASSWORD('6969Cutlass!!')"
-export M2_HOME=/usr/share/maven
-/usr/local/tomcat/bin/startup.sh
+#service mysql start 
+#mysql -e "SET PASSWORD FOR 'root'@'localhost'=PASSWORD('6969Cutlass!!')"
+
+
